22c8df91fabae271785cffd6ce14732f721eaadb
Rider Kirkpatrick <73277718+riderkirk@users.noreply.github.com>
2026-04-20 04:11:43 -0500

    Add: validate-parentheses - submission-7

---
+++ Data Structures & Algorithms/validate-parentheses/submission-7.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isValid(const string& s) {
+        std::stack<char> st;
+        for (char c : s) {
+            if (c == ')' || c == '}' || c == ']') {
+                char expected = 
+                    (c == ')') ? '(' :
+                    (c == ']') ? '[' : '{';
+                if (!st.empty() && st.top() == expected) {
+                    st.pop();
+                } else {
+                    return false;
+                }
+            } else {
+                st.push(c);
+            }
+        }
+        return st.empty();
+    }
+};
